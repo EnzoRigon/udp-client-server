d3731f4e010461b015289ff0053dea3783d5ae25
Enzo Rigon <rossiporto1509@gmail.com>
2024-10-09 17:32:56 -0300

    Update Dockerfile to use Python 3.12-slim image

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,8 @@
-# Use the official Ubuntu image from the Docker Hub
-FROM ubuntu:22.04
+# Use the official Python image from the Docker Hub
+FROM python:3.12-slim
 
 # Set the working directory inside the container
 WORKDIR /app
-
-# Install system dependencies
-RUN apt-get update && apt-get install -y \
-    wget \
-    gnupg \
-    software-properties-common \
-    python3 \
-    python3-pip \
-    python-is-python3 \
-    bison \
-    build-essential \
-    cmake \
-    flex \
-    git \
-    libedit-dev \
-    libllvm11 \
-    llvm-11-dev \
-    libclang-11-dev \
-    zlib1g-dev \
-    libelf-dev \
-    libfl-dev \
-    python3-distutils \
-    clang \
-    checkinstall \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
-
-# Add the BCC repository and install BCC tools
-RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD && \
-    echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/iovisor.list && \
-    apt-get update && \
-    apt-get install -y bcc-tools libbcc-examples linux-headers-$(uname -r)
-
-# Copy the requirements file to the working directory
-COPY requirements.txt .
-
-# Install the Python dependencies
-RUN pip3 install --no-cache-dir -r requirements.txt
 
 # Copy the rest of the application code to the working directory
 COPY . .
--- udp.py
+++ udp.py
@@ -2,8 +2,8 @@ import socket
 import threading
 import argparse
 import time
-from bcc import BPF
 
+#run docker: docker run --rm -it --privileged udp;
 
 # Global variable to control the interval for sending periodic messages
 report_send_interval = 5
@@ -22,12 +22,16 @@ def get_local_ip():
     try:
         # Create a UDP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
         # Connect to a public DNS server to determine the local IP address
         sock.connect(("8.8.8.8", 80))
+
         # Get the local IP address from the socket
         ip = sock.getsockname()[0]
+
         # Close the socket
         sock.close()
+
         return ip
     except Exception as e:
         # Print the error and return the loopback address if an exception occurs
@@ -60,6 +64,7 @@ def server(server_ip, port):
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((server_ip, port))
+
     print(f"UDP server is waiting for messages at {server_ip}:{port}...")
 
     while True:
@@ -68,6 +73,7 @@ def server(server_ip, port):
         response = f"{address} Sent: \n {response}"
         if address not in clients:
             clients.add(address)
+
         for client in clients:
             sock.sendto(response.encode(), client)
 
@@ -83,33 +89,37 @@ def receive_messages(sock):
         data = data.decode()
 
         try:
-            metrics = process_metrics(data)
-            print(f"Metrics of {address}: {metrics}")
+            data = int(data)
+            report_send_interval = data
+            print(f"Report send interval updated to: {report_send_interval}")
         except ValueError:
-            print(f"Error processing metric: {data}")
-
-
-def process_metrics(data):
-    if "CPU Usage" in data or "Context Switches" in data:
-        return f"Received: {data}"
-    return "Unknown metric"
+            print(f"\n{data} ")
 
 
 def send_periodic_message(sock, server_ip, port):
+    """
+    Function to send a periodic message "test" every report_send_interval seconds.
+    """
     global report_send_interval
     while True:
         time.sleep(report_send_interval)
-        cpu_usage, num_processes, context_switches = collect_metrics()
-        message = f"CPU Usage: {cpu_usage:.2f}%, Processes: {num_processes}, Context Switches: {context_switches}"
+        #TODO: change this to the computer report
+        message = "Olá"
         sock.sendto(message.encode(), (server_ip, port))
 
 
 def client(server_ip, port):
+    """
+    Start the client to send and receive messages.
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Start a thread to receive messages
     receive_thread = threading.Thread(target=receive_messages, args=(sock,))
     receive_thread.daemon = True
     receive_thread.start()
 
+    # Start a thread to send periodic messages
     periodic_thread = threading.Thread(target=send_periodic_message, args=(sock, server_ip, port))
     periodic_thread.daemon = True
     periodic_thread.start()
@@ -120,6 +130,9 @@ def client(server_ip, port):
 
 
 def start_server_with_check(server_ip, port):
+    """
+    Start the server if it is not already running.
+    """
     if not is_server_running(server_ip, port):
         server_thread = threading.Thread(target=server, args=(server_ip, port))
         server_thread.daemon = True
@@ -129,71 +142,10 @@ def start_server_with_check(server_ip, port):
         print("The server is already running, it will not be started again.")
 
 
-def collect_metrics():
-    # Define BPF program for CPU usage and context switch collection
-    bpf_program = """
-    BPF_HASH(start, u32);
-    BPF_HASH(total, u32);
-    BPF_HASH(context_switches, u64);  // Changed to u64 to avoid overflow
-
-    TRACEPOINT_PROBE(sched, sched_switch) {
-        u32 pid = bpf_get_current_pid_tgid();
-        u64 ts = bpf_ktime_get_ns();
-
-        // Increment context switch count
-        u64 zero = 0;
-        u64 *cs = context_switches.lookup_or_init(&zero, &zero);  // Use u64 here
-        (*cs)++;
-
-        // Calculate CPU usage
-        u64 *tsp = start.lookup(&pid);
-        if (tsp) {
-            u64 delta = ts - *tsp;
-            u64 *total_time = total.lookup_or_init(&pid, &delta);
-            *total_time += delta;
-        }
-        
-        start.update(&pid, &ts);
-        return 0;
-    }
-    """
-
-    # Load eBPF program
-    b = BPF(text=bpf_program)
-
-    while True:
-        try:
-            time.sleep(5)
-            total_cpu_usage = 0
-            process_count = 0
-            context_switches = 0
-
-            # Process CPU usage
-            for pid, val in b["total"].items():
-                cpu_usage = val.value / (5 * 1e9) * 100
-                total_cpu_usage += cpu_usage
-                process_count += 1
-
-            # Process context switches
-            for _, val in b["context_switches"].items():
-                context_switches = val.value
-
-            # Calculate average CPU usage
-            if process_count > 0:
-                average_cpu_usage = total_cpu_usage / process_count
-            else:
-                average_cpu_usage = 0
-
-            # Clear BPF maps for next cycle
-            b["total"].clear()
-            b["context_switches"].clear()
-
-            return average_cpu_usage, process_count, context_switches
-        except KeyboardInterrupt:
-            break
-
-
 def main(args):
+    """
+    Main function to start the server and client.
+    """
     server_ip = args.ip if args.ip else get_local_ip()
     port = args.port
 
@@ -209,6 +161,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="UDP server and client")
     parser.add_argument('--ip', type=str, help='Server IP')
     parser.add_argument('--port', type=int, default=5005, help='Server port')
+
     args = parser.parse_args()
 
     main(args)
